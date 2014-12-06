15ee349a561f6459dd1c9448039f928604aba9b9
Sean Allred <code@seanallred.com>
2014-12-06 01:43:37 -0500

    Confirm kill emacs

--- .emacs.d/.custom.el
+++ .emacs.d/.custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(confirm-kill-emacs (quote yes-or-no-p))
  '(find-file-hook
    (quote
     (auto-insert
