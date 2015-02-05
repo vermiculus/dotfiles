d0d29e522e72308e988cde7a1aa7da34f582d342
Sean Allred <code@seanallred.com>
2015-02-05 15:17:08 -0600

    Move and disable auto-minor-mode
    It was causing problems with bootstrapping.  Once everything is nice and
    ready as far as installed packages, the use-package form can be
    re-enabled (if desired).  The reason this fails is because
    `package-refresh-contents' uses `find-file' (presumably) to do its
    thing.  Since the function was on a hook, I couldn't find any files
    until this function was defined!
    
    It's likely the *real* fix in this commit is the custom-file change.  I
    may re-enable the hook at a later time.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,32 +62,9 @@
 
 ;; Auto-Minor-Mode
 
-;; http://stackoverflow.com/a/13946304/1443496
-(defvar *-auto-minor-mode-alist ()
-  "Alist of filename patterns vs correpsonding minor mode functions,
-see `auto-mode-alist'. All elements of this alist are checked,
-meaning you can enable multiple minor modes for the same
-regexp.")
-
-(defun *-enable-minor-mode-based-on-extension ()
-  "check file name against `*-auto-minor-mode-alist' to enable
-minor modes the checking happens for all pairs in
-`*-auto-minor-mode-alist'"
-  (when buffer-file-name
-    (let ((name buffer-file-name)
-          (remote-id (file-remote-p buffer-file-name))
-          (alist *-auto-minor-mode-alist))
-      (setq name (file-name-sans-versions name))
-      (when (and (stringp remote-id)
-                 (string-match-p (regexp-quote remote-id) name))
-        (setq name (substring name (match-end 0))))
-      (while (and alist (caar alist) (cdar alist))
-        (if (string-match (caar alist) name)
-            (funcall (cdar alist) 1))
-        (setq alist (cdr alist))))))
-
-(add-hook 'find-file-hook
-          #'*-enable-minor-mode-based-on-extension)
+(use-package auto-minor-mode
+  :disabled t
+  :load-path "my-packages/")
 
 
 ;; Creating Temporary Files
